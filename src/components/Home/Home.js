dfaed822f6df55d9dd52adca6514d12e31947368
SOUMYA CHAUDHARY <schaudhary332001@gmail.com>
2022-07-03 17:50:36 +0530

    removing the changes in name

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> SOUMYACHAUDHARY</strong>
+                <strong className="main-name"> SOUMYA CHAUDHARY</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
